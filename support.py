6fd5d77d304b0d08fa8a0ea22edb59fc18d5d57d
facelessuser <faceless.shop@gmail.com>
2017-11-19 15:25:45 -0700

    Doc related updates

--- support.py
+++ support.py
@@ -18,6 +18,31 @@ div.easy-diff { padding: 10px; margin: 0; }
 .easy-diff a { text-decoration: none; }
 '''
 
+frontmatter = {
+    "markdown_extensions": [
+        "markdown.extensions.admonition",
+        "markdown.extensions.attr_list",
+        "markdown.extensions.def_list",
+        "markdown.extensions.nl2br",
+        # Smart quotes always have corner cases that annoy me, so don't bother with them.
+        {"markdown.extensions.smarty": {"smart_quotes": False}},
+        "pymdownx.betterem",
+        {
+            "pymdownx.magiclink": {
+                "repo_url_shortener": True,
+                "repo_url_shorthand": True,
+                "user": "facelessuser",
+                "repo": "EasyDiff"
+            }
+        },
+        "pymdownx.extrarawhtml",
+        "pymdownx.keys",
+        {"pymdownx.escapeall": {"hardbreak": True, "nbsp": True}},
+        # Sublime doesn't support superscript, so no ordinal numbers
+        {"pymdownx.smartsymbols": {"ordinal_numbers": False}}
+    ]
+}
+
 
 def list2string(obj):
     """Convert list to string."""
@@ -131,8 +156,11 @@ class EasyDiffDocCommand(sublime_plugin.WindowCommand):
 
         try:
             import mdpopups
+            import pymdownx
             has_phantom_support = (mdpopups.version() >= (1, 10, 0)) and (int(sublime.version()) >= 3124)
+            fmatter = mdpopups.format_frontmatter(frontmatter) if pymdown.version[:3] >= (4, 3, 0) else ''
         except Exception:
+            fmatter = ''
             has_phantom_support = False
 
         if not has_phantom_support:
@@ -148,7 +176,7 @@ class EasyDiffDocCommand(sublime_plugin.WindowCommand):
                     view,
                     'quickstart',
                     sublime.Region(0),
-                    text,
+                    fmatter + text,
                     sublime.LAYOUT_INLINE,
                     css=CSS,
                     wrapper_class="easy-diff",
@@ -167,8 +195,11 @@ class EasyDiffChangesCommand(sublime_plugin.WindowCommand):
         """Show the changelog in a new view."""
         try:
             import mdpopups
+            import pymdownx
             has_phantom_support = (mdpopups.version() >= (1, 10, 0)) and (int(sublime.version()) >= 3124)
+            fmatter = mdpopups.format_frontmatter(frontmatter) if pymdown.version[:3] >= (4, 3, 0) else ''
         except Exception:
+            fmatter = ''
             has_phantom_support = False
 
         text = sublime.load_resource('Packages/EasyDiff/CHANGES.md')
@@ -181,7 +212,7 @@ class EasyDiffChangesCommand(sublime_plugin.WindowCommand):
                 view,
                 'changelog',
                 sublime.Region(0),
-                text,
+                fmatter + text,
                 sublime.LAYOUT_INLINE,
                 wrapper_class="easy-diff",
                 css=CSS,
